110793b1ac763ce40610b9e374204fec05f4fbc2
valentina buoro <valentinabuoro4@gmail.com>
2024-02-06 22:41:19 +0100

    wip

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -1,34 +1,74 @@
 // Sidebar.tsx
 
 import React from "react";
-import {  usePathname } from "next/navigation";
-
-
+import { usePathname } from "next/navigation";
 
 const MobileNavbar: React.FC = () => {
 
+    const [color, setColor] = React.useState(false)
+    function changeColor(){
+        if(window.scrollY >= 100){
+            setColor(true)
+        }else{
+            setColor(false)
+        }
+    }
+    window.addEventListener('scroll', changeColor)
+
+
+  const [active, setActive] = React.useState(false);
   const scrollToSection = (id: any) => {
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
+      setActive(!active);
     }
   };
   const pathname = usePathname();
 
-  const activeRoute = pathname === "/tech-stack" 
+  const activeRoute = pathname === "/tech-stack";
 
   return (
-    <nav className="bg-[#18151B] h-12 w-11/12 fixed top-4 left-0 overflow-y-auto mx-4 shadow-sm md:hidden">
-       <div className="flex justify-around items-center text-sm p-4">
-        <span className="block"><a  onClick={() => scrollToSection('home')}>Home</a></span>
-        <span className="block"><a  onClick={() => scrollToSection('projects')}>Projects</a></span>
-        <span className="block"><a  onClick={() => scrollToSection('tech-stack')}>Tech Stack</a></span>
-        <span className="block"><a  onClick={() => scrollToSection('services')}>Services</a></span>
-        <span className="block"><a  onClick={() => scrollToSection('blog')}>Blog</a></span>
-        <span className="block"><a  onClick={() => scrollToSection('contact')}>Contact</a></span>
-       
-       </div>
-      
+    <nav className={color? "bg-[#4D0071]/40 z-30 h-12 w-11/12 fixed top-4 left-0 overflow-y-auto mx-4 py-4 px-0 rounded-2xl shadow-sm md:hidden": "bg-[#4D0071] z-30 h-12 w-11/12 fixed top-4 left-0 overflow-y-auto mx-4 py-4 px-0 rounded-2xl shadow-sm md:hidden"}>
+      <div className="flex justify-around items-center text-sm ">
+        <span className={`li`}>
+          <a onClick={() => scrollToSection("home")}>Home</a>
+        </span>
+        <span className="li active:border-b-white">
+          <a onClick={() => scrollToSection("projects")}>Projects</a>
+        </span>
+        <span className="li">
+          <a onClick={() => scrollToSection("tech-stack")}>Tech Stack</a>
+        </span>
+        <span className="li">
+          <a onClick={() => scrollToSection("services")}>Services</a>
+        </span>
+        <span className="li">
+          <a onClick={() => scrollToSection("blog")}>Blog</a>
+        </span>
+        <span className="li">
+          <a onClick={() => scrollToSection("contact")}>Contact</a>
+        </span>
+      </div>
+      <style jsx>{`
+        .li {
+          margin: auto;
+          color: #ffffff;
+          font-size: 14px;
+          cursor: pointer;
+        }
+        .li::after {
+          content: "";
+          display: block;
+          border-bottom: solid 3px #4d0071;
+          padding-bottom: 1%;
+          transform: scaleX(0);
+          transition: transform 300ms ease-in-out;
+        }
+        .li:hover:after {
+          transform: scaleX(1);
+        }
+      `}</style>
     </nav>
   );
 };
