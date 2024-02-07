1b17a440923a5b535e88e8ceea64639359be9949
valentina buoro <valentinabuoro4@gmail.com>
2024-02-07 20:38:39 +0100

    wip

--- data/project-data.tsx
+++ data/project-data.tsx
@@ -1,11 +1,11 @@
 import TrustBallot from "../public/trustballot.png";
-import Ballot from "../public/trustballot.png";
+import Meta from "../public/metabnb.png";
 
 export const projectFeed = [
   {
     id:"1",
     img: TrustBallot,
-    title: "on;",
+    title: "Trust Ballot",
     description: "Prisma and Next.js go _great_ together!",
     github: "https://github.com/queen-x-vee/Hackathon-WIBAHacks",
     deployed: "https://wibateam4.netlify.app/",
@@ -13,10 +13,10 @@ export const projectFeed = [
   },
   {
     id:"2",
-    img: Ballot,
-    title: "Prisma is the perfect ORM for Next.js",
+    img: Meta,
+    title: "MetaBNB",
     description: "Prisma and Next.js go _great_ together!",
-    github: "https://github.com/prisma/prisma",
-    deployed: "https://github.com/prisma/prisma",
+    github: "https://github.com/queen-x-vee/hng-metabnb-app",
+    deployed: "https://bigteenz-metabnb.netlify.app/",
   },
 ];
