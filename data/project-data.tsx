5e739275e511c76e23c6ddd1ffa201c66573f41a
valentina buoro <valentinabuoro4@gmail.com>
2024-05-02 00:05:57 +0100

    afrimed project addition

--- data/project-data.tsx
+++ data/project-data.tsx
@@ -1,6 +1,7 @@
 import TrustBallot from "../public/trustballot.png";
 import Meta from "../public/metabnb.png";
 import Innovotio from "../public/innovotio.png";
+import Afrimed from "../public/afrimed.png"
 
 export const projectFeed = [
   {
@@ -18,13 +19,13 @@ export const projectFeed = [
   },
   {
     id:"2",
-    img: TrustBallot,
-    title: "Trust Ballot",
-    description: "Trust Ballot is a blockchain webapp that allows users to create elections and vote on them. It is built with Next.js, Etherjs, and Tailwind CSS.",
-    github: "https://github.com/queen-x-vee/Hackathon-WIBAHacks",
-    deployed: "https://wibateam4.netlify.app/",
-    firstTool: "NextJS",
-    secondTool: "EtherJS",
+    img: Afrimed,
+    title: "Afrimed",
+    description: "Afrimed is a web app which applies Artificial Intelligence for diagnostic health services. It is built with React.js, Zustand, and Tailwind CSS.",
+    github: "https://github.com/valentina-buoro/afrimed-frontend",
+    deployed: "https://afrimed-app.netlify.app/",
+    firstTool: "ReactJS",
+    secondTool: "Zustand",
     thirdTool: "Tailwind CSS",
     fourthTool: "JavaScript",
 
@@ -49,6 +50,19 @@ export const mainProjectFeed = [
   },
   {
     id:"2",
+    img: Afrimed,
+    title: "Afrimed",
+    description: "Afrimed is a web app which applies Artificial Intelligence for diagnostic health services. It is built with React.js, Zustand, and Tailwind CSS.",
+    github: "https://github.com/valentina-buoro/afrimed-frontend",
+    deployed: "https://afrimed-app.netlify.app/",
+    firstTool: "ReactJS",
+    secondTool: "Zustand",
+    thirdTool: "Tailwind CSS",
+    fourthTool: "JavaScript",
+
+  },
+  {
+    id:"3",
     img: TrustBallot,
     title: "Trust Ballot",
     description: "Trust Ballot is a blockchain webapp that allows users to create elections and vote on them. It is built with Next.js, Etherjs, and Tailwind CSS.",
@@ -59,17 +73,5 @@ export const mainProjectFeed = [
     thirdTool: "Tailwind CSS",
     fourthTool: "JavaScript",
 
-  },
-  {
-    id:"3",
-    img: Meta,
-    title: "MetaBNB",
-    description: "Prisma and Next.js go _great_ together!",
-    github: "https://github.com/queen-x-vee/hng-metabnb-app",
-    deployed: "https://bigteenz-metabnb.netlify.app/",
-    firstTool: "ReactJS",
-    secondTool: "React Router",
-    thirdTool: "Tailwind CSS",
-    fourthTool: "JavaScript",
   },
 ];
